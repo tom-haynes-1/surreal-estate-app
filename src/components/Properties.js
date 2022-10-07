5a5547217be8e7ab816cb644512a00b706253976
Tom Haynes <tomhaynes94@gmail.com>
2022-10-07 11:15:21 +0100

    added Properties component and which render by retrieving the data from an axios GET request. Need to style PropertyCard component so it displays in a nice format

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,10 +1,41 @@
-import React from 'react';
+/* eslint-disable no-console */
+/* eslint-disable arrow-body-style */
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import PropertyCard from './PropertyCard';
+import Alert from './Alert';
 import '../styles/properties.css';
 
-const Properties = () => (
-  <div className="properties-title-container">
-    <h2>View a Property</h2>
-  </div>
-);
+const Properties = () => {
+  const initialState = {
+    properties: [],
+  };
+
+  const [properties, setProperties] = useState(initialState.properties);
+  const [alert, setAlert] = useState({ message: '', isSuccess: false });
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/api/v1/PropertyListing/')
+      .then(({ data }) => setProperties(data))
+      .catch((error) => {
+        console.log(error);
+        setAlert({
+          message: 'Error adding property. Please try again later.',
+          isSucces: false,
+        });
+      });
+  }, []);
+
+  return (
+    <div className="properties-title-container">
+      <h2>View a Property</h2>
+      <Alert message={alert.message} success={alert.isSuccess} />
+      {properties.map((property) => (
+        <PropertyCard {...property} />
+      ))}
+    </div>
+  );
+};
 
 export default Properties;
